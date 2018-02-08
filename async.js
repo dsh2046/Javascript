db60a3bf33ef102d7728430827f2a7c90a97ecda
Samuel <dsh20462046@gmail.com>
2018-02-08 16:32:04 -0500

    Update async.js

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-NO.1 Ajax
+1 Ajax
 
 ajax1(x(), function(data1){
     ajax2(y(data1), function(data2){
@@ -9,7 +9,7 @@ ajax1(x(), function(data1){
 })
 
 
-NO.2 Promise
+2 Promise
 
 var request1 = function(){
     return new Promise(function(resolve, reject){
@@ -47,7 +47,7 @@ request1()
     }).catch()
   
 
-NO.3  Async, Await
+3  Async, Await
 
 async function request(){
   var data1 = await request1().catch()
